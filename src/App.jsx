fe3e5482b9ef0dd11b57c82d3587ac972c4198c9
Nikita Fedotov <nikfedotoff@mail.ru>
2022-10-26 11:36:07 +0600

    feat: animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,47 @@
-import React, { Suspense, useRef, useState } from 'react'
+import React, { Suspense, useRef, useState, useEffect } from 'react'
 import './App.css'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
+import { SheetProvider, editable as e } from '@theatre/r3f'
 import Office from './models/Office'
 import studio from '@theatre/studio'
 import extension from '@theatre/r3f/dist/extension'
+import { getProject } from '@theatre/core'
+import demoProjectState from './jsons/scene1.json'
 
+const demoSheet = getProject('Office', { state: demoProjectState }).sheet(
+    'Office'
+)
 studio.initialize()
 studio.extend(extension)
 
+if (import.meta.env.DEV) {
+    studio.initialize()
+    studio.extend(extension)
+}
+
 function App() {
+    useEffect(() => {
+        demoSheet.sequence.play({ iterationCount: Infinity, range: [0, 10] })
+    }, [])
+
     return (
-        <Canvas camera={{ position: [0, 2, 5], zoom: 1 }}>
-            <OrbitControls />
-            <color attach="background" args={['lightblue']} />
-            <hemisphereLight intensity={0.35} />
-            <spotLight
-                position={[10, 10, 10]}
-                angle={0.3}
-                penumbra={1}
-                intensity={2}
-                castShadow
-            />
-            <Suspense fallback={null}>
-                <Office />
-            </Suspense>
+        <Canvas camera={{ position: [-50, -12, 3], zoom: 1 }}>
+            <SheetProvider sheet={demoSheet}>
+                <OrbitControls />
+                <color attach="background" args={['lightblue']} />
+                <hemisphereLight intensity={0.35} />
+                <spotLight
+                    position={[10, 10, 10]}
+                    angle={0.3}
+                    penumbra={1}
+                    intensity={2}
+                    castShadow
+                />
+                <Suspense fallback={null}>
+                    <Office />
+                </Suspense>
+            </SheetProvider>
         </Canvas>
     )
 }
